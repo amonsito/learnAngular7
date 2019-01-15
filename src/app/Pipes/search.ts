2b61a3984cc0696bf56cd7a54b61f34e3a28cfb1
juan.avila <juan_da89@hotmail.com>
2019-01-15 07:46:36 -0500

    subiendo pipes

--- src/app/Pipes/search.ts
+++ src/app/Pipes/search.ts
@@ -5,6 +5,7 @@ import{Pipe,PipeTransform} from '@angular/core';
 })
 export class SearchPipe implements PipeTransform{
     public transform(value,args:string){
+        console.log(value,args)
         if(!value){
             return;
         }
